663bb8369373ecae1186d4a83beed216947113f9
Eli Bendersky <eliben@gmail.com>
2022-06-22 05:34:32 -0700

    Make Digits's functions methods on the type
    The type needs to be changed to an actual standalone type instead of an
    alias to a builtin (on which methods could not be defined)

--- digits.go
+++ digits.go
@@ -2,7 +2,7 @@ package sudoku
 
 // Digits represents a set of possible digits for a Sudoku square. The functions
 // in this file perform set operations on Digits, as needed for Sudoku.
-type Digits = uint16
+type Digits uint16
 
 // Internal representation: digit N is represented by the Nth lowest bit in
 // the Digits value, e.g.:
@@ -12,11 +12,11 @@ type Digits = uint16
 // has the bits N=1,5,6 are set, so it represents the set of digits {1, 5, 6}
 
 // isMember checks whether digit n is a member of the digit set d.
-func isMember(d Digits, n uint16) bool {
+func (d Digits) isMember(n uint16) bool {
 	return (d & (1 << n)) != 0
 }
 
 // add adds digit n to set d and returns the new set.
-func add(d Digits, n uint16) Digits {
+func (d Digits) add(n uint16) Digits {
 	return d | (1 << n)
 }
--- digits_test.go
+++ digits_test.go
@@ -10,11 +10,11 @@ func TestIsMember(t *testing.T) {
 	var i uint16
 	for i = 1; i <= 9; i++ {
 		if i == 1 || i == 5 || i == 6 {
-			if !isMember(d, i) {
+			if !d.isMember(i) {
 				t.Errorf("got isMember(%016b, %d)=false, want true", d, i)
 			}
 		} else {
-			if isMember(d, i) {
+			if d.isMember(i) {
 				t.Errorf("got isMember(%016b, %d)=true, want false", d, i)
 			}
 		}
@@ -25,12 +25,12 @@ func TestAdd(t *testing.T) {
 	d := Digits(0b0000000001100010)
 
 	wantd2 := Digits(0b0000000001100110)
-	if add(d, 2) != wantd2 {
-		t.Errorf("got wantd2=%v, want=%v", add(d, 2), wantd2)
+	if d.add(2) != wantd2 {
+		t.Errorf("got wantd2=%v, want=%v", d.add(2), wantd2)
 	}
 
 	wantd5 := Digits(0b0000000001100010)
-	if add(d, 5) != wantd5 {
-		t.Errorf("got wantd5=%v, want=%v", add(d, 5), wantd5)
+	if d.add(5) != wantd5 {
+		t.Errorf("got wantd5=%v, want=%v", d.add(5), wantd5)
 	}
 }

bf5ad84b2b8dd5acbd26a4f8dee9673f5893e32a
Eli Bendersky <eliben@gmail.com>
2022-06-23 10:24:50 -0700

    Initial code for "solver" binary

--- cmd/solver/main.go
+++ cmd/solver/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/eliben/go-sudoku"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	flag.Parse()
+
+	filename := flag.Args()[0]
+
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Expect one board per line
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		board := strings.TrimSpace(scanner.Text())
+		if len(board) == 0 {
+			continue
+		}
+
+		v, err := sudoku.SolveBoard(board)
+		if err != nil {
+			log.Fatalf("unable to parse/solve board:", err)
+		}
+
+		fmt.Println(v)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }

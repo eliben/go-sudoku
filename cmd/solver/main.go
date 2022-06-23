651a6c7b85d499c1b5ef05ca1ff403a7b8cb402f
Eli Bendersky <eliben@gmail.com>
2022-06-23 10:36:28 -0700

    Report timings in the solver

--- cmd/solver/main.go
+++ cmd/solver/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/eliben/go-sudoku"
 )
@@ -20,23 +21,43 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	var totalDuration time.Duration = 0
+	var maxDuration time.Duration = 0
+	var numBoards int = 0
+
 	// Expect one board per line
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		board := strings.TrimSpace(scanner.Text())
-		if len(board) == 0 {
+		if len(board) == 0 || strings.HasPrefix(board, "#") {
 			continue
 		}
 
+		numBoards++
+
+		tStart := time.Now()
 		v, err := sudoku.SolveBoard(board)
+		tElapsed := time.Now().Sub(tStart)
 		if err != nil {
 			log.Fatalf("unable to parse/solve board:", err)
 		}
 
-		fmt.Println(v)
+		totalDuration += tElapsed
+		if tElapsed > maxDuration {
+			maxDuration = tElapsed
+		}
+
+		if !sudoku.IsSolved(v) {
+			log.Fatalf("board %v is not solved", v)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("Total boards solved:", numBoards)
+	fmt.Println("Average duration:", totalDuration/time.Duration(numBoards))
+	fmt.Println("Max duration:", maxDuration)
 }

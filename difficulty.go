b72b984f53bb7468900a49bfb7975eb9009dca4e
Eli Bendersky <eliben@gmail.com>
2022-06-27 06:04:40 -0700

    Count min bound on hints in row/col

--- difficulty.go
+++ difficulty.go
@@ -22,6 +22,42 @@ func EvaluateDifficulty(values Values) (int, error) {
 
 	fmt.Println("hintcount before elimination:", countHits())
 
+	// Count the lower bound (minimal number) of hints in rows and cols, pre
+	// elimination.
+	minHints := 9
+
+	index := func(row, col int) Index {
+		return row*9 + col
+	}
+
+	// ... first the rows.
+	for row := 0; row < 9; row++ {
+		rowCount := 0
+		for col := 0; col < 9; col++ {
+			if values[index(row, col)].Size() == 1 {
+				rowCount++
+			}
+		}
+		if rowCount < minHints {
+			minHints = rowCount
+		}
+	}
+
+	// ... then the columns.
+	for col := 0; col < 9; col++ {
+		colCount := 0
+		for row := 0; row < 9; row++ {
+			if values[index(row, col)].Size() == 1 {
+				colCount++
+			}
+		}
+		if colCount < minHints {
+			minHints = colCount
+		}
+	}
+
+	fmt.Println("min hints:", minHints)
+
 	if !EliminateAll(values) {
 		return 0, fmt.Errorf("contradiction in board")
 	}
--- difficulty_test.go
+++ difficulty_test.go
@@ -7,9 +7,10 @@ import (
 
 var h1 = "85...24..72......9..4.........1.7..23.5...9...4...........8..7..17..........36.4."
 var h2 = "..53.....8......2..7..1.5..4....53...1..7...6..32...8..6.5....9..4....3......97.."
+var h3 = "030050040008010500460000012070502080000603000040109030250000098001020600080060020"
 
 func TestEvaluateDifficulty(t *testing.T) {
-	v, err := ParseBoard(h2, false)
+	v, err := ParseBoard(h3, false)
 	fmt.Println(DisplayAsInput(v))
 	if err != nil {
 		t.Fatal(err)

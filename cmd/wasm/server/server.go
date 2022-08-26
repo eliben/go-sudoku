2a3313be55bc5ba348ec8beb04d0cd201af93be0
Eli Bendersky <eliben@gmail.com>
2022-08-26 15:44:36 -0700

    Added Makefile and fine-tuned HTML UI

--- cmd/wasm/server/server.go
+++ cmd/wasm/server/server.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-	err := http.ListenAndServe(":8899", http.FileServer(http.Dir("cmd/wasm/assets")))
+	port := ":8899"
+	fmt.Println("Serving on", port)
+
+	err := http.ListenAndServe(port, http.FileServer(http.Dir("assets")))
 	if err != nil {
 		fmt.Println("Failed to start server", err)
 		return
